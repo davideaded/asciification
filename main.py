d9affce284160517ce53ef6c9c03671e2e66780d
davideaded <david.castrodc033@gmail.com>
2025-06-11 19:19:06 -0300

    refactoring and acepting cli arguments

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from PIL import Image, ImageFilter
-import math
+#!/usr/bin/python3
+from PIL import Image, ImageEnhance
+import math, sys, shutil
 
-ACCEPTABLE_FORMATS = ("JPEG", "JPG", "WEBP")
+ACCEPTABLE_FORMATS = ("jpeg", "jpg", "webp")
+
+if len(sys.argv) < 2:
+    print(f"[ERROR] {len(sys.argv)} arguments passed. Usage: {sys.argv[0]} <file>")
+    sys.exit(1)
+
+format = sys.argv[1].split(".")[-1].lower()
+if not format in ACCEPTABLE_FORMATS:
+    print(f"[ERRO] Format {format} still not supported")
+    sys.exit(1)
 
 def debug_img(img):
     print(f"DEBUGGING {img.filename}")
@@ -11,56 +21,45 @@ def debug_img(img):
 def resize_img(img, max_width):
     aspect_ratio = img.height / img.width
     new_width = min(img.width, max_width)
-    new_height = int(aspect_ratio * new_width * 0.50)
+    new_height = int(aspect_ratio * new_width * 0.55)
     return img.resize((new_width, new_height))
+
 
 def get_grayscale_from_img(filename, max_width):
     try:
         with Image.open(filename) as im:
             debug_img(im)
-            if im.format not in(ACCEPTABLE_FORMATS):
-                print(f"[WARN] Format {im.format} still not supported")
-                return None
-
             im = resize_img(im, max_width)
+            im = im.convert("L")
 
-            pixels = im.load()
+            pixels = list(im.getdata())
             width, height = im.size
-            gray_scaled_pixels = []
-            for y in range(height):
-                for x in range(width):
-                    cpixel = pixels[x, y]
-                    lum = math.floor((0.3 * cpixel[0]) + (0.59 * cpixel[1]) + (0.11 * cpixel[2]))
-                    gray_scaled_pixels.append(lum)
 
-            return gray_scaled_pixels, width, height
+            return pixels, width, height
 
     except FileNotFoundError:
         print(f"[ERROR] File not found: {filename}")
     except Exception as e:
         print(f"[ERROR] loading the image: {e}")
 
-
-# mapping luminosity to only two chars: @ and .
-
 def print_ascii(filename, max_width=100):
     res = get_grayscale_from_img(filename, max_width)
     if res == None:
         return
     gs, w, h = res 
+    shades = "@%#*+=-:. "
 
     ascii_art = []
     for y in range(h):
         line = ""
-        shades = "@#%. "
         for x in range(w):
             px_value = gs[y * w + x]
             line += shades[px_value * len(shades) // 256]
         ascii_art.append(line + "\n")
 
-    print("Done")
-
     for line in ascii_art:
         print(line)
 
-print_ascii("testimg/sp.jpg", 50)
+filename = sys.argv[1]
+terminal_width = shutil.get_terminal_size()[0]
+print_ascii(filename, terminal_width // 2)
